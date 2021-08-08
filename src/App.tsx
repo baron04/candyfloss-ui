562aa3e9674263d2a5ef6ca4fdfa5c1c164f776d
baron1994 <1942992735@qq.com>
2021-08-09 00:42:15 +0800

    改用npm link测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import React from "react";
-import ButtonTest from "./test/ButtonTest";
-import MenuTest from "./test/MenuTest";
-import IconTest from "./test/IconTest";
-import UploadTest from "./test/UploadTest";
 
 const App: React.FC = () => {
   return (
-    <div className="App" style={{paddingBottom: '30px'}}>
-      Edit <code>src/App.tsx</code> and save to reload.
-      <ButtonTest />
-      <hr />
-      <MenuTest />
-      <IconTest />
-      <UploadTest />
-    </div>
+    <div></div>
   );
 };
 
